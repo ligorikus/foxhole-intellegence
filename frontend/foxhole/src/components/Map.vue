84776cea54bad2677cc5bf07fda7b7a2f6af0b5a
Ligorikus <semal551@gmail.com>
2023-10-07 14:53:01 +0500

    Init project

---
+++ frontend/foxhole/src/components/Map.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+//https://glitch.com/edit/#!/foxhole-tilemap-prototype
+const count = ref(0)
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
