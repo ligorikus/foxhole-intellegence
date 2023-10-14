96420e8047edbbeb19f8df8530f02a8d22a3f800
Ligorikus <semal551@gmail.com>
2023-10-14 09:28:39 +0500

    leaflet init

--- frontend/foxhole/src/App.vue
+++ frontend/foxhole/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Map />
 </template>
 
 <style scoped>
--- frontend/foxhole/src/components/Map.vue
+++ frontend/foxhole/src/components/Map.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+const zoom = ref(6)
 
-defineProps<{ msg: string }>()
-//https://glitch.com/edit/#!/foxhole-tilemap-prototype
-const count = ref(0)
 </script>
 
 <template>
+    <div style="height:600px; width:800px">
+        <l-map :useGlobalLeaflet="false" ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+            <l-tile-layer
+            url="https://raw.githubusercontent.com/Kastow/Foxhole-Map-Tiles/master/Tiles/{z}/{z}_{x}_{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+            ></l-tile-layer>
+        </l-map>
+    </div>
 </template>
 
-<style scoped>
+<style>
+.leaflet-control-attribution {
+    display: none !important;
+}
 </style>
